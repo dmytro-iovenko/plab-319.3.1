9811228a614ca1af42e7f89a9a160a27bc179a5c
Dmytro Iovenko <iovenko@gmail.com>
2024-09-10 23:55:52 -0400

    add logging

--- index.js
+++ index.js
@@ -4,5 +4,20 @@ import db from "./db.js";
 
 const port = 3000;
 
+// Parsing Body Middleware
+app.use(express.json())
+
+// Logging Middleware
+app.use((req, res, next) => {
+  const time = new Date().toLocaleString();
+  console.log("------");
+  console.log(`${time}: Received a ${req.method} request to ${req.url}.`);
+  if (Object.keys(req.body).length > 0) {
+    console.log("Containing the data:");
+    console.log(JSON.stringify(req.body));
+  }
+  next();
+});
+
 // Run express server
 app.listen(port, () => console.log("Server is running on port:", port));
